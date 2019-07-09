faaea1503490104b8911cbeaf32909d2eb3cf68a
Septian Feri Fedinan <feri.ferdinan79@gmail.com>
2019-07-09 21:35:30 +0700

    edit router

---
+++ src/navigations/Router.js
@@ -0,0 +1,41 @@
+import { createSwitchNavigator, createStackNavigator, createAppContainer ,createMaterialTopTabNavigator} from 'react-navigation';
+
+import LoadingScreen from "../screens/Loading/Loading"
+import LoginScreen from "../screens/Login/index"
+import RegisterScreen from "../screens/Register/index"
+import HomeScreen from "../screens/Home/index"
+import CrosswodScreen from "../screens/Crosswod/index"
+
+
+
+const AppStack = createStackNavigator({ 
+  Home: HomeScreen, 
+  Crosswod: CrosswodScreen, 
+},{
+  headerMode: 'none',
+  navigationOptions: {
+  headerVisible: false,
+},});
+const AuthStack = createStackNavigator({ Login: LoginScreen, Register:RegisterScreen },{
+  headerMode: 'none',
+  navigationOptions: {
+  headerVisible: false,
+},});
+const AuthLoadingScreen = createStackNavigator({Loading:LoadingScreen },{
+  headerMode: 'none',
+  navigationOptions: {
+  headerVisible: false,
+},});
+
+
+
+export default createAppContainer(createSwitchNavigator(
+  {
+    AuthLoading: AuthLoadingScreen,
+    App: AppStack,
+    Auth: AuthStack,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  }
+));
