b30d991aa36151178b8df4937068c0d9977aa1d3
Septian Feri Fedinan <feri.ferdinan79@gmail.com>
2019-07-11 23:32:28 +0700

    menu

--- src/screens/Home/Index.js
+++ src/screens/Home/Index.js
@@ -1,20 +1,10 @@
-import React, { Component } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TouchableOpacity,
-  FlatList,
-  Dimensions,
-  Alert,
-  ScrollView,
-  AsyncStorage
-} from "react-native";
-import { Right,Left,Header } from "native-base";
 import { Icon } from "react-native-elements";
 import { Menu, MenuOption , MenuOptions , MenuTrigger } from 'react-native-popup-menu';
 import {withNavigation} from 'react-navigation';
+const axios = require('axios');
+import configs from '../../../config'
+import * as actionCrosswords from '../../redux/action';
+import { connect } from 'react-redux';
 
 
 class HomeScreen extends Component {
@@ -22,6 +12,8 @@ class HomeScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      menu:[],
+      token:'',
       modalVisible: false,
       userSelected: [],
       data: [
@@ -34,8 +26,31 @@ class HomeScreen extends Component {
       ]
     };
   }
+  async componentDidMount(){
+    that = this
+    const valueToken= await AsyncStorage.getItem('token')
+    this.setState({
+      token:valueToken
+    })
+    let config = {
+      headers: {
+        'Authorization': 'bearer ' + that.state.token
+      }
+    }
+    axios.get(`http://${configs.BASE_URL}:3333/api/crosswords`,config)
+    .then(function (response) {
+      console.log(response.data)
+      that.setState({
+        menu:response.data
+      })
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 
-  
+    
+}
+
   handleLogout = () =>{
     AsyncStorage.clear()
     this.props.navigation.navigate('Login')
@@ -69,6 +84,8 @@ class HomeScreen extends Component {
   };
 
   render() {
+    console.log('ini props home',this.props);
+    
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
@@ -76,16 +93,16 @@ class HomeScreen extends Component {
         <FlatList
           style={styles.contentList}
           columnWrapperStyle={styles.listContainer}
-          data={this.state.data}
-          keyExtractor={item => {
-            return item.id;
-          }}
+          data={this.state.menu}
+          keyExtractor={(item, index) => (`menu-${index}`)}
           renderItem={({ item }) => {
             return (
               <TouchableOpacity
                 style={styles.card}
-                onPress={() => {this.props.navigation.navigate('Crosswod')}}>
-                {item.is_finished==1?
+                onPress={() => {this.props.navigation.navigate('Crosswod',{
+                            crosswordId:item.pivot.id,
+                        })}}>
+                {item.pivot.is_finished==1?
                 (<Icon name="check-circle" color='green' size={40} />):
                 (<View style={styles.circle}></View>)}
                 <View style={styles.cardContent}>
@@ -101,55 +118,22 @@ class HomeScreen extends Component {
   }
 }
 
-export default withNavigation(HomeScreen)
+const mapStateToProps = state => {
+  return {
+    menu: state.menu
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    menu: () => dispatch(actionCrosswords.menu())
+  }
+}
+
+export default withNavigation(connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(HomeScreen))
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#ebf0f7"
-  },
-  contentList: {
-    flex: 1,
-  },
-  cardContent: {
-    marginLeft: 20,
-    marginTop:5
-  },
-  circle: {
-    borderWidth: 2,
-    width:36,
-    height:36,
-    borderRadius:36/2
-  },
-
-  card: {
-    shadowColor: "#00000021",
-    shadowOffset: {
-      width: 0,
-      height: 6
-    },
-    shadowOpacity: 0.37,
-    shadowRadius: 7.49,
-    elevation: 12,
-
-    margin: 10,
-    backgroundColor: "white",
-    padding: 10,
-    flexDirection: "row",
-    borderRadius: 30
-  },
-
-  name: {
-    fontSize: 18,
-    flex: 1,
-    alignSelf: "center",
-    color: "#000",
-    fontWeight: "bold"
-  },
-  count: {
-    fontSize: 14,
-    flex: 1,
-    alignSelf: "center",
-    color: "#6666ff"
-  },
-});
